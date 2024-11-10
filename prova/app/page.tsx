342253875bd6a8add8530ab777f92074354808a5
JackLoop <80691990+JacklO0p@users.noreply.github.com>
2024-11-10 15:45:56 +0100

    prova modifica

--- prova/app/page.tsx
+++ prova/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <section className="container w-full h-full">
+      <div>Test</div>
+    </section>
   );
 }
